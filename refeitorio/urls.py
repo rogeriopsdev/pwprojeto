303e930a8adcbdba0bedb8f1b724d72e0362d2cf
rogeriopsdev <rogerio.pereira@ifto.edu.br>
2022-05-12 16:26:21 -0300

    aula1205

--- refeitorio/urls.py
+++ refeitorio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from refeitorioapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
 ]
