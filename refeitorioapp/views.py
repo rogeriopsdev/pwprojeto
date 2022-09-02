8b41e6230f59d08527ba0f67afbdc67e4340f648
aluno <rogerio.pereira@ifto.edu.br>
2022-09-02 11:22:10 -0300

    Versão2_2

--- refeitorioapp/views.py
+++ refeitorioapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from refeitorioapp.forms import MesaForms
 
+
 # Create your views here.
 def index(request):
-    return render(request,'refeitorio/index.html')
+    return render(request, 'refeitorio/index.html')
+
 
 def criar_mesa(request):
-    form = MesaForms()
-    return render(request,'refeitorio/criar_mesa.html',{'form':form})
-
+    form = MesaForms(request.POST)
+    if request.method == "POST":
+        form = MesaForms(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            obj.save()
+            form = MesaForms()
+    return render(request, 'refeitorio/criar_mesa.html', {'form': form})
